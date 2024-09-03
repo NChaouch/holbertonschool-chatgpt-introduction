bdc26c605aaf6868d3753a73ce45e7cc9320169f
NChaouch <nourchaouch44@gmail.com>
2024-09-03 13:39:40 +0000

    pycodestyle for task 0

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import sys
 
+
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1 # Decrementing n to not infenitely while loop
+        n -= 1  # Decrementing n to not infenitely while loop
     return result
+
 
 f = factorial(int(sys.argv[1]))
 print(f)
